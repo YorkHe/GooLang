d9536bc7f69a2afc05f4b7c28e4d3eb2adfd1b6e
York <kiddmagician@gmail.com>
2016-08-10 20:42:21 +0800

    Initial commit

---
+++ main.js
@@ -0,0 +1,35 @@
+/**
+ *
+ * Created by 宇 on 2016/8/10.
+ */
+
+
+$(function(){
+    var language = "简体中文";
+
+    function getLanguage(language){
+        var dict = {
+            "简体中文" : "lang_zh-CN",
+            "繁體中文" : "lang_zh-TW",
+            "日本語" : "lang_ja",
+            "English": "lang_en"
+        };
+
+        if (dict[language] != undefined)
+            return dict[language];
+        else
+            return dict["简体中文"];
+    }
+
+    $(".dropdown-menu a").click(function(){
+        console.log(this.text);
+        $("#language").text(this.text);
+        language = this.text;
+    })
+
+    $("#btn-search").click(function(){
+        var search_text = $("#search_content").val();
+        window.open("https://www.google.com/webhp#lr="+getLanguage(language) + "&q=" + search_text);
+    })
+});
+
